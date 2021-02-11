ec82ea9a61ef109663b7b4d8aae90db93ee51c2a
Philip <62082747+philip-marshall@users.noreply.github.com>
2021-02-10 22:22:24 -0700

    Added a single line for ESLint to ignore undefined stuff. This will be removed when we start adding our own code to the file

--- src/settings/dialog.js
+++ src/settings/dialog.js
@@ -1,7 +1,9 @@
+
 (function(){
     'use strict';
   
     // The Office initialize function must be run each time a new page is loaded.
+    // eslint-disable-next-line no-undef
     Office.initialize = function(reason){
       jQuery(document).ready(function(){
         if (window.location.search) {
