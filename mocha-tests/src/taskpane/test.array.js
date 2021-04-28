a69a83f6bd0588dc8f1354a3ce66e500ccb56b59
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 20:19:18 -0600

    Update test.array.js
    Added another test and some descriptions

--- mocha-tests/src/taskpane/test.array.js
+++ mocha-tests/src/taskpane/test.array.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var assert = chai.assert;
-
+//Generic test that empty array should have length of 0
 describe('Array', function() {
   it('should start empty', function() {
     var arr = [];
@@ -8,3 +8,17 @@ describe('Array', function() {
     assert.equal(arr.length, 0);
   });
 });
+
+//Generic Test that compares arrays have same members - Taken from https://stackoverflow.com/questions/44335770/node-mocha-chai-unit-tests-compare-array-of-objects-regardless-of-order
+var a = [
+    {name: 'Sean', age: 35},
+    {name: 'Weston', age: 30},
+]
+
+//assume we got the result in this order
+var b = [
+    {name: 'Sean', age: 30},
+    {name: 'Weston', age: 35},
+]
+
+expect(a).to.have.deep.members(b)
