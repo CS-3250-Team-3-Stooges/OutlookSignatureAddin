f7f5ee785fc0d3b45d0a82fc8d4945be1fd505ff
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 22:18:08 -0600

    Updated Math Testing
    additional tests for abs

--- mocha-tests/src/commands/test.commands.js
+++ mocha-tests/src/commands/test.commands.js
@@ -10,8 +10,16 @@ describe('Math', function() {
     describe('#abs()', function() {
         it('should return positive value of given negative number', function() {
             expect(Math.abs(-5)).to.be.equal(5);
-         });
-      });
+        });
+
+        it('should return positive value of given positive number', function() {
+            expect(Math.abs(3)).to.be.equal(3);
+        });
+
+        it('should return 0 given 0', function() {
+            expect(Math.abs(0)).to.be.equal(0);
+        });
+    });
 //Code by Phillip - Reformatted by Sean - Expected multiplication test
 describe('#multiplyNumbers()', function () {
     it('should return the result of multiplication', function () {
