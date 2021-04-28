d3e836154d58ef99233efc9fc805f1dd93164523
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 20:50:42 -0600

    Update test.array.js
    added descriptions

--- mocha-tests/src/taskpane/test.array.js
+++ mocha-tests/src/taskpane/test.array.js
@@ -2,7 +2,8 @@ var chai = require('chai');
 var assert = chai.assert; // using Assert Style
 var expect = chai.expect; //Using Expect Style
 var should = chai.should; // Using Should Style
-//Generic test that empty array should have length of 0
+
+//Code by Sean - Generic test that empty array should have length of 0
 describe('ArrayEmpty', function() {
   it('should start empty', function() {
     var arr = [];
@@ -11,7 +12,7 @@ describe('ArrayEmpty', function() {
   });
 });
 
-//Generic Test that compares arrays have same members - Taken from https://stackoverflow.com/questions/44335770/node-mocha-chai-unit-tests-compare-array-of-objects-regardless-of-order
+//Code adapted by Sean - Generic Test that compares arrays have same members - Taken from https://stackoverflow.com/questions/44335770/node-mocha-chai-unit-tests-compare-array-of-objects-regardless-of-order
 describe('ArrayComparison', function() {
   it('should have the same members', function() {
    var a = [
@@ -25,7 +26,7 @@ describe('ArrayComparison', function() {
    {name: 'Weston', age: 30},
    ]
 
-   expect(a).to.have.deep.members(b) 
-    
+   expect(a).to.have.deep.members(b)
+
   });
 });
