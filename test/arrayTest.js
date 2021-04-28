9b58821aafdcb2f8186e66f4d9f812b8da378dd0
dragar99 <49768103+dragar99@users.noreply.github.com>
2021-04-27 19:41:43 -0600

    Add files via upload

---
+++ test/arrayTest.js
@@ -0,0 +1,16 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+  describe('Random signature', function () {
+    it('should return a string', function () {
+      var Randomint = signatureList[1];
+
+      assert.equal(Weston- "\"Dude, suckin' at something is the first step at being sorta good at something.\"<br>-  Jake <small><em>(Adventure Time)</em></small>",);
+    });
+});
