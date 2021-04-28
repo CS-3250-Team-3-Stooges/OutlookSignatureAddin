bef6bdead26e77c42eb89581373210eaf43870f0
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 20:49:32 -0600

    Update test.taskpane.js
    Added Description

--- mocha-tests/src/taskpane/test.taskpane.js
+++ mocha-tests/src/taskpane/test.taskpane.js
@@ -3,7 +3,7 @@ var assert = chai.assert; // using Assert Style
 var expect = chai.expect; //Using Expect Style
 var should = chai.should; // Using Should Style
 
-//Generic test that empty array should have length of 0
+//Code by Sean and Weston - Generic test that created array has the correct number of members
 describe('ArrayLength', function() {
   it('should contain 3 members', function() {
     var accountList = ["Philip", "Sean", "Weston"];
