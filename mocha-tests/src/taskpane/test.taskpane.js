f0056eb6340172455bd73732615d818f32c29790
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 22:29:07 -0600

    Update test.taskpane.js
    Testing plug-and-play code - pretty sure it's gonna break!

--- mocha-tests/src/taskpane/test.taskpane.js
+++ mocha-tests/src/taskpane/test.taskpane.js
@@ -1,9 +1,40 @@
+global.Office = () => ({});
+global.Office.onReady = () => ({});
+
 var chai = require('chai');
 var assert = chai.assert; // using Assert Style
 var expect = chai.expect; //Using Expect Style
 var should = chai.should; // Using Should Style
 
 
+describe('apply Signature test', () => {
+    it('title should not be null',()=>{
+    const title = window.document.getElementById('signature');
+    expect(title).toBeDefined();
+    });
+    it('email Message Should equal singature with title',()=>{
+        const _title = 'hello';
+        const singatureList= [
+        {title: 'hello', message: 'Message'}
+        ]
+        let _message = null;
+        singatureList.forEach(({title, message})=>{
+        if (_title === title)_message = message;
+        });
+        expect(_message).toEqual('Message');
+        });
+        it ('message should be null',()=>{});
+        });
+        
+var add = require ('./taskpane');
+
+describe('Apply Random Signature from the list', () => {
+    test('Should be signature message apply random mailbox', () => {
+        const result = add.signatureList[add.getRandom()-1]
+        expect(result.length).toBeGreaterThanOrEqual*(0) ;
+
+    });
+});
 //Code by Sean - Generic test that empty array should have length of 0
 describe('Array', function() {
   it('should start empty', function() {
@@ -27,6 +58,7 @@ describe('Array', function() {
 
    expect(a).to.have.deep.members(b)
   });
+});
 
   //Code by Sean and Weston - Generic test that created array has the correct number of members
   it('should contain 3 members', function() {
