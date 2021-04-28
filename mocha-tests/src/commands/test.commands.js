82a2fbf367ab2bcc88eba8e103c85f837f7f0173
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 21:30:48 -0600

    Update test.commands.js
    Added Class Testing via sitepoint tutorial

--- mocha-tests/src/commands/test.commands.js
+++ mocha-tests/src/commands/test.commands.js
@@ -1,4 +1,3 @@
-
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 const multiplyNumbers = require('../../../src/commands/commands.js').multiplyNumbers;
@@ -13,3 +12,38 @@ describe('Commands', function () {
         });
     });
 });
+
+//Added by Sean - Code taken from https://www.sitepoint.com/unit-test-javascript-mocha-chai/
+describe('addClass', function() {
+  it('should add class to element', function() {
+    var element = { className: '' };
+
+    addClass(element, 'test-class');
+
+    assert.equal(element.className, 'test-class');
+  });
+
+  it('should not add a class which already exists');
+});
+
+it('should not add a class which already exists', function() {
+  var element = { className: 'exists' };
+
+  addClass(element, 'exists');
+
+  var numClasses = element.className.split(' ').length;
+  assert.equal(numClasses, 1);
+});
+
+function addClass(el, newClass) {
+  if(el.className.indexOf(newClass) !== -1) {
+    return;
+  }
+
+  if(el.className !== '') {
+    //ensure class names are separated by a space
+    newClass = ' ' + newClass;
+  }
+
+  el.className += newClass;
+}
