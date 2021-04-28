f97e9654595f154d77c3c587f087505c94fa045b
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 21:05:37 -0600

    Update test.taskpane.js
    Merged Array Tests into single file

--- mocha-tests/src/taskpane/test.taskpane.js
+++ mocha-tests/src/taskpane/test.taskpane.js
@@ -3,8 +3,33 @@ var assert = chai.assert; // using Assert Style
 var expect = chai.expect; //Using Expect Style
 var should = chai.should; // Using Should Style
 
-//Code by Sean and Weston - Generic test that created array has the correct number of members
-describe('ArrayLength', function() {
+
+//Code by Sean - Generic test that empty array should have length of 0
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+
+  //Code adapted by Sean - Generic Test that compares arrays have same members - Taken from https://stackoverflow.com/questions/44335770/node-mocha-chai-unit-tests-compare-array-of-objects-regardless-of-order
+  it('should have the same members', function() {
+   var a = [
+   {name: 'Sean', age: 35},
+   {name: 'Weston', age: 30},
+   ]
+
+   //assume we got the result in this order
+   var b = [
+   {name: 'Sean', age: 35},
+   {name: 'Weston', age: 30},
+   ]
+
+   expect(a).to.have.deep.members(b)
+  });
+});
+
+  //Code by Sean and Weston - Generic test that created array has the correct number of members
   it('should contain 3 members', function() {
     var accountList = ["Philip", "Sean", "Weston"];
 
