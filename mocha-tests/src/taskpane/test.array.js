7ee8d242690e7f6f18763ba138b697bae5f6e113
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 20:36:25 -0600

    Update test.array.js
    updated code for comparison test

--- mocha-tests/src/taskpane/test.array.js
+++ mocha-tests/src/taskpane/test.array.js
@@ -3,7 +3,7 @@ var assert = chai.assert; // using Assert Style
 var expect = chai.expect; //Using Expect Style
 var should = chai.should; // Using Should Style
 //Generic test that empty array should have length of 0
-describe('Array', function() {
+describe('ArrayEmpty', function() {
   it('should start empty', function() {
     var arr = [];
 
@@ -12,15 +12,20 @@ describe('Array', function() {
 });
 
 //Generic Test that compares arrays have same members - Taken from https://stackoverflow.com/questions/44335770/node-mocha-chai-unit-tests-compare-array-of-objects-regardless-of-order
-var a = [
-    {name: 'Sean', age: 35},
-    {name: 'Weston', age: 30},
-]
+describe('ArrayComparison', function() {
+  it('should have the same members', function() {
+   var a = [
+   {name: 'Sean', age: 35},
+   {name: 'Weston', age: 30},
+   ]
 
-//assume we got the result in this order
-var b = [
-    {name: 'Sean', age: 35},
-    {name: 'Weston', age: 30},
-]
+   //assume we got the result in this order
+   var b = [
+   {name: 'Sean', age: 35},
+   {name: 'Weston', age: 30},
+   ]
 
-expect(a).to.have.deep.members(b)
+   expect(a).to.have.deep.members(b) 
+    
+  });
+});
