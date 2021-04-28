0617a0cfcdc22c89b2b177e3c360bd617dfdcf3b
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-04-27 21:40:48 -0600

    Update test.commands.js
    Added import module testing

--- mocha-tests/src/commands/test.commands.js
+++ mocha-tests/src/commands/test.commands.js
@@ -2,6 +2,8 @@ const expect = require('chai').expect;
 const assert = require('chai').assert;
 const multiplyNumbers = require('../../../src/commands/commands.js').multiplyNumbers;
 const randomSignature = require('../../../src/commands/commands.js').randomSignature;
+var className = require('../src/commands/test.className.js');
+var addClass = className.addClass;
 
 //Code by Phiilip - Expected multiplication test
 describe('Commands', function () {
