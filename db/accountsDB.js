5ee1949bfe644f86487560d6abb85241a115e9e0
Jacob Torres <jacob@jacobtorres.net>
2021-04-29 19:39:53 -0600

    Wrote insert and remove SQL queries and partially refactored createAcctTable function.

--- db/accountsDB.js
+++ db/accountsDB.js
@@ -6,28 +6,32 @@
  */
 
 
-// Instantiate new sqlite3 db
+// Instantiate new sqlite3 database
 const sqlite3 = require('sqlite3').verbose();
-const db_file = 'accountsDB.sqlite3';
+const db_file = 'AccountsDB.sqlite3';
 
 // SQL queries
-const CREATE_TABLE = `create table accounts
-  (name text, email text, signatures text);`
-const INSERT_ACCOUNT = ``
+const CREATE_TABLE = `create table Accounts
+  (AcctID integer primary key autoincrement,
+    Name text,
+    Email text,
+    Signatures text,
+    Selected integer,
+    Remove integer);`
+const INSERT_ACCOUNT = `insert into Accounts
+  values (?, ?, ?, ?, ?, ?);`
+const REMOVE_ACCOUNT = `
+  delete from Accounts where Remove == 1;`
 
-export function createDB() {
+export function createAcctTable() {
 
   // Open new connection
   let db = new sqlite3.Database(db_file, (err) => {
     if (err) {
       return console.error(err.message);
     }
-    console.log('Successfully connected to the accounts database.');
+    console.log('Successfully connected to the Accounts database.');
   });
-}
-
-
-export function createTable() {
 
   // Create the account table with 3 columns: name, email, and signatures
   db.run(CREATE_TABLE, (err) => {
@@ -48,5 +52,10 @@ export function createTable() {
 
 
 export function insertAccount() {
-  
+
+  // Prepare SQL statement to insert new account(s)
+  insertStmt = db.prepare(
+
+  );
+
 }
