396c61c26876adab7bc0f105016e732a8425dc81
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-05-08 15:51:43 -0600

    Update taskpane.js
    Updated JSDoc format

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,11 +1,18 @@
 var accountList = ["Philip", "Sean", "Weston"];
 
+/** Documented by Sean
+* Represents an account selection function
+* @constructor
+*/
 (function(){
   'use strict';
   var config;
   var signatureList;
 
-  /** Code by Philip */
+  /** Documented by Sean
+  *
+  *@var {signature} signature - The signature from a text file  
+  *Code by Philip */
   Office.onReady($.when(getSignatures("signature")).then(storeSignatures));
 
 
@@ -15,7 +22,9 @@ var accountList = ["Philip", "Sean", "Weston"];
 
       config = getConfig();
 
-      /** Code by Phillip - Functionality for account selection via dropdown menu within taskpane */
+      /** Documented by Sean
+      *
+      * Code by Phillip - Functionality for account selection via dropdown menu within taskpane */
       if(isAccountSelected == false){
       buildAccountList('#dropdown-menu', accountList, signatureList);
       $('#account-selection').toggle(true);
@@ -25,7 +34,10 @@ var accountList = ["Philip", "Sean", "Weston"];
       /** Code by Philip - Insert Signature button functionality for taskpane */
       $('#insert-signature').on('click', function(){
 
-        /** Deprecated - var signature = getSelectedSignature(); */
+        /** Documented by Sean
+        * @var {signature} - signature from text file
+        * @deprecated
+        * Deprecated - var signature = getSelectedSignature(); */
         var signature = signatureList[getRadioID()];
         var selectedSignature = "<br> " + signature + " <br>";
         Office.context.mailbox.item.body.setSelectedDataAsync(selectedSignature, {coercionType: 'html'});
@@ -34,7 +46,10 @@ var accountList = ["Philip", "Sean", "Weston"];
 
 
       $('#random-signature').on('click', function(){
-        /** Code by Sean and Philip */
+        /** Documented by Sean 
+        *@var {randomNumber} - random index from the signature list
+        *@borrows {floor) - from random class to change random number into integer for grabbing indices
+        * Code by Sean and Philip */
             var accountSigs = getAccountIndices(getAccount());
             console.log(accountSigs);
             /** Code by Sean - floor method to round to integer for random function to grab index of list or array */
@@ -45,24 +60,30 @@ var accountList = ["Philip", "Sean", "Weston"];
             Office.context.mailbox.item.body.setSelectedDataAsync(randomSignature, {coercionType: 'html'});
       });
 
-      /** Code by Philip - Save signature button functionality */
+      /** Documented by Sean
+      *Code by Philip - Save signature button functionality */
       $('#save-signature').on('click', function(){
-        /** variable for new signature */
+        /** Documented by Sean
+        *@var {newSig} - variable for new signature */
         var newSig = $('#new-signature').val();
 
         $.ajax({
           url: "https://localhost:3000/set-signature?newSignature=" + getAccount() + "-" + newSig,
           type: "GET"
         })
-        /** Build signature list functionality */
+        /** Documented by Sean
+        *Build signature list functionality */
         buildSignatureList('#signatures-list', signatureList);
       })
 
 
-      /** Written by Jose with the help of Weston - Delete signature from list functionality */
+      /** Documented by Phillip
+      *Written by Jose with the help of Weston - Delete signature from list functionality */
       $('#delete-signature').on('click', function(){
 
-/** - Deprecated code -
+/** Documented by Sean
+*@deprecated 
+- Deprecated code -
         var signID = - 1;
         var deleteSig;
         var radioButtons = document.getElementsByName('signature-radio');
@@ -81,13 +102,18 @@ var accountList = ["Philip", "Sean", "Weston"];
         url: "https://localhost:3000/delete-signature?deleteSignature=" + getRadioID() /*signID*/,
         type: "GET"
       });
-        /** Build signature list functionality */
+        /** Documented by Sean
+        *@constructor
+        *@param {signatureList} - created list from #signatures-list
+        *@param {#signatures-list} - pulled from text file
+        *Build signature list functionality */
         buildSignatureList('#signatures-list', signatureList);
       });
     });
   };
 
-  /** Code by Philip - Functionality to store signature and split by the newline character */
+  /**Documented by Phillip 
+  *Code by Philip - Functionality to store signature and split by the newline character */
   function storeSignatures(demsigs)
   {
     console.log(demsigs);
