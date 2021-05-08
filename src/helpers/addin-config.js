2735fe409a43fcd3fcb63b1c0fa470cf38c480d0
Sean Kruse <77900483+xDazed@users.noreply.github.com>
2021-05-08 16:32:38 -0600

    Update addin-config.js
    Update JSDoc format

--- src/helpers/addin-config.js
+++ src/helpers/addin-config.js
@@ -1,3 +1,6 @@
+/** Documented by Sean
+ *@constuctor
+ */
 function getConfig() {
     var config = {};
   
@@ -6,7 +9,13 @@ function getConfig() {
   
     return config;
   }
-  
+
+ /** Documented by Sean
+ *@constuctor
+ *@param {string} config - office configuration paramater
+ *@param {string} callback - callback function as paramater
+ *@callback
+ */
   function setConfig(config, callback) {
     Office.context.roamingSettings.set('gitHubUserName', config.gitHubUserName);
     Office.context.roamingSettings.set('defaultGistId', config.defaultGistId);
